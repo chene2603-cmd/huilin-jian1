dcf7436d3850705e3ddb6c9167930e4793dbc5ad
 chene2603-cmd  <chene2603@gmail.com>
2026-05-10 20:35:26 +0800

    Update engine.h

--- engine/engine.h
+++ engine/engine.h
@@ -3,10 +3,10 @@
 
 #include "core/core_types.h"
 
-/* 引擎生命周期 */
-void engine_init(void);
-void engine_update(void);
-void engine_shutdown(void);
-bool engine_is_running(void);
+void  engine_init(void);
+void  engine_update(void);
+void  engine_shutdown(void);
+bool  engine_is_running(void);
+void* engine_get_window(void);
 
-#endif /* ENGINE_H */
+#endif
